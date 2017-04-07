7a51f5885d24386982462e43af38c00bf7662610
Tim Gabets <tim@gabets.ru>
2017-04-07 15:33:52 +0300

    Balance Enquiry

--- py8583/isoClient.py
+++ py8583/isoClient.py
@@ -12,28 +12,28 @@ from isoTools import trace, get_datetime
 from py8583spec import IsoSpec, IsoSpec1987BPC
 from terminal import Terminal
 
-def balance_enquiry(term):
+def balance_enquiry(terminal_id, merchant_id, currency_code):
     """
     Balance Enguiry
     """
     IsoMessage = ISO8583(IsoSpec=IsoSpec1987BPC())            
     IsoMessage.MTI("0100")
         
-    IsoMessage.FieldData(2, 5555011012400477)
+    IsoMessage.FieldData(2, 5678011012400477)
     IsoMessage.FieldData(3, 310000)
     IsoMessage.FieldData(4, 0)
     IsoMessage.FieldData(7, get_datetime())
     IsoMessage.FieldData(11, random.randint(0, 999999))
-    IsoMessage.FieldData(12, 104956)
+    #IsoMessage.FieldData(12, 104956)
     IsoMessage.FieldData(13, 1216)
     IsoMessage.FieldData(22, 20)
     IsoMessage.FieldData(22, 20)
     IsoMessage.FieldData(24, 100)
     IsoMessage.FieldData(25, 0)
-    IsoMessage.FieldData(35, '4290011012400477=18091011872300000720')
-    IsoMessage.FieldData(41, term.get_terminal_id())
-    IsoMessage.FieldData(42, term.get_merchant_id())    
-    IsoMessage.FieldData(49, term.get_currency_code())
+    IsoMessage.FieldData(35, '5678011012400477=18091011872300000720')
+    IsoMessage.FieldData(41, terminal_id)
+    IsoMessage.FieldData(42, merchant_id)
+    IsoMessage.FieldData(49, currency_code)
 
     IsoMessage.Print()
     return IsoMessage.BuildIso()
@@ -57,7 +57,23 @@ def echo_test(terminal_id, merchant_id):
 
 
 def main(term):
+    """
+    # Echo
     data = echo_test(term.get_terminal_id(), term.get_merchant_id())
+    data = struct.pack("!H", len(data)) + data
+             
+    trace('>> {} bytes sent:'.format(len(data)), data)
+    term.send(data)
+
+    data = term.recv()
+    trace('<< {} bytes received: '.format(len(data)), data)
+
+    IsoMessage = ISO8583(data[2:], IsoSpec1987BPC())
+    IsoMessage.Print()
+    """
+
+    # Trxn
+    data = balance_enquiry(term.get_terminal_id(), term.get_merchant_id(), term.get_currency_code())
     data = struct.pack("!H", len(data)) + data
              
     trace('>> {} bytes sent:'.format(len(data)), data)
